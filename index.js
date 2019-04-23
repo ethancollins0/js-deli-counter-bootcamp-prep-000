cc498129a3ed32b3de10ba08be2a79348835f533
Ethan Collins <ethantcollins98@gmail.com>
2019-04-23 07:09:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
 function takeANumber(line, name) {
-  line.push(name) //adds name to end of array
+  line.push(name)
   return `Welcome, ${name}. You are number ${line.length} in line.`
-} //above line interpolates name and number and returns the string
+}
 
-function nowServing(line) {
-  if (line.length > 0) { //if there are people in line
-    var a = line[0] //set a = to value of the first item in array
-    line.shift() //remove the first item from the array
-    return `Currently serving ${a}.` //return the shifted value
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return `Currently serving ${katzDeliLine.shift()}.`
   } else {
     return "There is nobody waiting to be served!"
   }
 }
 
 function currentLine(line) {
-  var str = "The line is currently: "
-  if (line.length > 0) { //if the array isn't empty
-    var i = 0
-    while (line.length > 0) { //while the array isn't empty
-      str += `${i + 1}. ${line[0]}` //formatting / adding strings
-      line.shift()
-      if (line.length > 0) {
-        str += ", " //formatting / adding strings
+  if (line.length > 0) {
+    var lineList = "The line is currently:"
+    for (var i = 0; i < line.length; i++) {
+      lineList += ` ${i+1}. ${line[i]}`
+      if (i < line.length - 1) { //i doesn't increment until after loop, so we
+        lineList += ","         //need to check if the list is about to be complete
       }
-      i++; //variable to track number in line
     }
-  } else {
-    return "The line is currently empty."
+    return lineList
   }
-  return str
+  return "The line is currently empty."
 }
