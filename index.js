3dcaf4089cc44f0036827dd44ece27d62706052b
Ethan Collins <ethantcollins98@gmail.com>
2019-04-23 07:39:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 function takeANumber(line, name) {
-  line.push(name)
-  return `Welcome, ${name}. You are number ${line.length} in line.`
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
-    return `Currently serving ${katzDeliLine.shift()}.`
+    return `Currently serving ${katzDeliLine.shift()}.`;
   } else {
-    return "There is nobody waiting to be served!"
+    return "There is nobody waiting to be served!";
   }
 }
 
 function currentLine(line) {
   if (line.length > 0) {
-    var lineList = "The line is currently:"
+    var lineList = "The line is currently:";
     for (let i = 0; i < line.length; i++) {
-      lineList += ` ${i+1}. ${line[i]}`
+      lineList += ` ${i+1}. ${line[i]}`;
       if (i < line.length - 1) { //i doesn't increment until after loop, so we
-        lineList += ","         //need to check if the list is about to be complete
+        lineList += ",";        //need to check if the list is about to be complete
       }
     }
     return lineList
