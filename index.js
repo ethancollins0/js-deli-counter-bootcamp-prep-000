70cb8fe2234739d3596c2360c4500122f66dc7c0
Ethan Collins <ethantcollins98@gmail.com>
2019-04-23 07:37:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nowServing(katzDeliLine) {
 function currentLine(line) {
   if (line.length > 0) {
     var lineList = "The line is currently:"
-    for (var i = 0; i < line.length; i++) {
+    for (let i = 0; i < line.length; i++) {
       lineList += ` ${i+1}. ${line[i]}`
       if (i < line.length - 1) { //i doesn't increment until after loop, so we
         lineList += ","         //need to check if the list is about to be complete
